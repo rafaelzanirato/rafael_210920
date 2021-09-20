59cec217cb8034dc2d9283ba9dfca0a33c8df94a
rafaelzanirato <84473402+rafaelzanirato@users.noreply.github.com>
2021-09-20 18:51:16 -0300

    Add files via upload

---
+++ rafael_210920.py
@@ -0,0 +1,11 @@
+print("Question 2 ************************")
+question= input("Write a phrase, please ")
+print("Question 3 ************************")
+for value in range(10):
+    print(question + "!")
+print("Question 4 ************************")
+pri= int(input("Can you say a number, please? "))
+bi= int(input("Another one? "))
+print("Question 5 ************************")
+print("The sum of these numbers is " + str(pri+bi))
+
